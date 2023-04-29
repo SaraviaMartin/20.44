9a9626bf1f830be63cee9c53e70fc2bf091bc074
Juan Saravia <saraviamartin16@hotmail.com>
2023-04-29 19:00:48 -0300

    changes

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner.jpg');
+		$(".banner-image").backstretch('images/metal-perforacion-maquina-industrial.jpg');
 		
 		// Fixed header
 		//-----------------------------------------------
